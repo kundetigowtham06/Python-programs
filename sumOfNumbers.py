a41a958bf91e6f712dc24f86be901cd08be845f4
kundetigowtham06 <152044248+kundetigowtham06@users.noreply.github.com>
2024-07-23 10:36:36 +0530

    Update sumOfNumbers.py

--- sumOfNumbers.py
+++ sumOfNumbers.py
@@ -5,3 +5,10 @@ while i<=n:
 	sum=sum+i
 	i+=1
 print("sum of first n natural numbers=",sum)
+#for loop
+n=int(input("Enter n="))
+sum=0
+for i in range(1,n+1):
+	sum=sum+i
+	
+print("sum of first 10 natural numbers=",sum)
