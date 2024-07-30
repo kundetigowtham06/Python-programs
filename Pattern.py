44c96bb3f009de60004accdd90abd73b6cf363f2
kundetigowtham06 <152044248+kundetigowtham06@users.noreply.github.com>
2024-07-30 11:00:44 +0530

    Update Pattern.py

--- Pattern.py
+++ Pattern.py
@@ -17,3 +17,11 @@ for i in range(1,n+1):
 		print(chr(m),end=" ")
 	m+=1
 	print("")
+	#Alphabet second pattern
+n=int(input("No.of rows="))
+m=65
+for i in range(1,n+1):
+	for j in range(1,i+1):
+		print(chr(m),end=" ")
+		m+=1
+	print("")
