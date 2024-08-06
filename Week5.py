5e3c0965478e6b1699c6afbd21a57e9753d4386a
kundetigowtham06 <152044248+kundetigowtham06@users.noreply.github.com>
2024-08-06 10:34:50 +0530

    Update Week5.py

--- Week5.py
+++ Week5.py
@@ -9,3 +9,14 @@ add=sum(l)
 avg=(add)/len(l)
 print("sum=",add)
 print("avg=",avg)
+# second
+l=[]
+n=int(input("No.of elements="))
+for i in range(n):
+	e=int(input("Enter numbers="))
+	l.append(e)
+#method 1
+print(l[::-1])
+#method 2
+l.reverse()
+print(l)
