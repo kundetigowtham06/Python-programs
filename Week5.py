0b200e2b1ab14e852e7d22283370b75035bcda47
kundetigowtham06 <152044248+kundetigowtham06@users.noreply.github.com>
2024-08-06 10:45:17 +0530

    Update Week5.py

--- Week5.py
+++ Week5.py
@@ -20,3 +20,16 @@ print(l[::-1])
 #method 2
 l.reverse()
 print(l)
+#first occurence
+l=[]
+n=int(input("No.of elements="))
+for i in range(n):
+	e=int(input("Enter numbers="))
+	l.append(e)
+m=len(l)-1
+t=l[0]
+l[0]=l[m]
+l[m]=t
+print(l)
+#method 2
+
