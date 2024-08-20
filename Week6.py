8bd02ecf0a720635a96cbe646597f17a94562a4b
kundetigowtham06 <152044248+kundetigowtham06@users.noreply.github.com>
2024-08-20 10:52:50 +0530

    Update Week6.py

--- Week6.py
+++ Week6.py
@@ -6,3 +6,35 @@ rev=""
 for i in s:
 	rev=i+rev
 print("Reverse=",rev)'''
+#palindrome
+'''s=input("Enter string= ")
+rev=""
+for i in s:
+	rev=i+rev
+if s==rev:
+	print("Palindrome")
+else:
+	print("Not a Palindrome")'''
+#length of string
+'''s=input("Enter string= ")
+count=0
+for i in s:
+	count+=1
+print("Length=",count)'''
+#number of vowels
+'''s=input("Enter string= ")
+c,v=0,0
+for i in s:
+	if i in "aeiouAEIOU":
+		v+=1
+	else:
+		c+=1
+print("No of vowels=",v)
+print("No of consonants=",c)'''
+#even length
+s=input("Enter String").split(" ")
+for i in s:
+	if len(i)%2==0:
+		print(i)
+
+
