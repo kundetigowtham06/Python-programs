82694b491230dcd5eabcbcd089ae93d225f43661
Gowtham_kundeti <152044248+kundetigowtham06@users.noreply.github.com>
2024-08-27 10:29:15 +0530

    Update Week7.py

--- Week7.py
+++ Week7.py
@@ -1,1 +1,18 @@
-
+#Add
+def sum(a,b):
+	return a+b
+add=sum(16,14)
+print("Addition is ",add)
+#Mul
+def mul(a,b,c):
+	return a*b*c
+print("Multiplication is",mul(1,2,3))
+#Sub
+def sub(a,b=4,c=9):
+	return a-b-c
+print("Subtraction is",sub(24))
+#div
+def div(a,b):
+	return a/b
+print("Divison is",int(div(b=2,a=6)))
+#program 2
