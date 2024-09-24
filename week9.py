8e31f706713c576d71cc79ff7c21dabdb577be37
Gowtham_kundeti <152044248+kundetigowtham06@users.noreply.github.com>
2024-09-24 11:17:04 +0530

    Update week9.py

--- week9.py
+++ week9.py
@@ -12,3 +12,16 @@ f2=open("gotam.bin",'wb')
 f2.write(b'\x00\x01')
 f2=open("gotam.bin",'rb')
 print(f2.read())
+#csv
+import csv
+d=[['s.no','name','roll'],
+[1,'gotam',7],
+[2,'hello',9]
+]
+with open('king.csv','w',newline='') as f:
+	m=csv.writer(f)
+	m.writerows(d)
+with open('king.csv','r') as f:
+	read=csv.reader(f)
+	for i in read:
+		print(i)
