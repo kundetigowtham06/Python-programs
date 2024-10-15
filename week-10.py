459b06cc4edc91c3c8cf5d99fa518cde5a66d7cc
Gowtham_kundeti <152044248+kundetigowtham06@users.noreply.github.com>
2024-10-15 11:17:29 +0530

    Update week-10.py

--- week-10.py
+++ week-10.py
@@ -13,3 +13,22 @@ class Bye(Hello):
 o=Bye()
 o.details(12,'ram')
 o.display()
+#multiple Inheritance
+class Hello:
+	branch='CSE'
+	def details(self,roll,name):
+		self.r=roll
+		self.n=name
+	def display(self):
+		print(self.r)
+		print(self.n)
+class Bye:
+	def __init__(self):
+		print("Hello world")
+class Child(Hello,Bye):
+	def Name(self):
+		print("Hello Python")
+o=Child()
+o.details(12,'ram')
+o.display()
+o.Name()
